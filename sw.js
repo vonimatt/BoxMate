5d7598c4e9b09a5008fccc34b2711872d6099012
vonimatt <vonimatt@me.com>
2026-05-25 12:09:32 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'boxmate-v4';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './css/styles.css',
+  './js/data.js',
+  './js/storage.js',
+  './js/generator.js',
+  './js/app.js',
+  './icons/icon.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
